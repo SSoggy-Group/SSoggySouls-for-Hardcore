739757f602b3e410c0d179fea5a797ef4787db3a
SSoggyTacoMan <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-01 10:10:02 +0200

    feat: add configurable head placement and dynamic grace period to HeadDropListener

--- fabric/src/main/java/org/ssoggy/ssoggysouls/hrm/HeadDropListener.java
+++ fabric/src/main/java/org/ssoggy/ssoggysouls/hrm/HeadDropListener.java
@@ -15,6 +15,8 @@ import org.ssoggy.ssoggysouls.SSoggySoulsMod;
 import org.ssoggy.ssoggysouls.database.DatabaseManager;
 import org.ssoggy.ssoggysouls.model.PlayerData;
 
+import org.ssoggy.ssoggysouls.util.ConfigManager;
+
 import java.util.concurrent.CompletableFuture;
 
 public class HeadDropListener {
@@ -26,35 +28,51 @@ public class HeadDropListener {
             // Check db asynchronously to avoid blocking death
             CompletableFuture.runAsync(() -> {
                 PlayerData data = db.getPlayer(player.getUuid());
-                if (data == null || !data.isDead() || data.isInGracePeriod(24 * 60 * 60 * 1000L)) {
+                if (data == null || !data.isDead() || data.isInGracePeriod(ConfigManager.parseGracePeriod(ConfigManager.getConfig().gracePeriod))) {
                     return; // Don't drop head if not dead or in grace
                 }
 
+                if (!ConfigManager.getConfig().dropHeads) {
+                    return;
+                }
+
                 // Drop head on the main thread
-                player.server.execute(() -> {
-                    World world = player.getServerWorld();
-                    BlockPos pos = player.getBlockPos();
+                    // Handle placing as block vs dropping as item
+                    if (ConfigManager.getConfig().headPlaceAsBlock) {
+                        BlockPos headPos = findSafeBlockPos(world, pos);
+                        world.setBlockState(headPos, net.minecraft.block.Blocks.PLAYER_HEAD.getDefaultState());
+                        net.minecraft.block.entity.BlockEntity be = world.getBlockEntity(headPos);
+                        if (be instanceof net.minecraft.block.entity.SkullBlockEntity skull) {
+                            skull.setOwner(new ProfileComponent(player.getGameProfile()));
+                            skull.markDirty();
+                        }
+                        SSoggySoulsMod.LOGGER.info("Placed {}'s head at {} {} {}", player.getName().getString(), headPos.getX(), headPos.getY(), headPos.getZ());
+                    } else {
+                        // Create player head item
+                        ItemStack head = new ItemStack(Items.PLAYER_HEAD);
+                        head.set(DataComponentTypes.PROFILE, new ProfileComponent(player.getGameProfile()));
+                        head.set(DataComponentTypes.CUSTOM_NAME, 
+                                Text.literal(player.getName().getString() + "'s Head")
+                                .styled(style -> style.withColor(Formatting.YELLOW)));
 
-                    // Create player head item
-                    ItemStack head = new ItemStack(Items.PLAYER_HEAD);
-                    
-                    // Set player profile (texture)
-                    head.set(DataComponentTypes.PROFILE, new ProfileComponent(player.getGameProfile()));
-                    
-                    // Set custom name
-                    head.set(DataComponentTypes.CUSTOM_NAME, 
-                            Text.literal(player.getName().getString() + "'s Head")
-                            .styled(style -> style.withColor(Formatting.YELLOW)));
-
-                    // Spawn item entity
-                    ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, head);
-                    // Make fireproof
-                    itemEntity.setInvulnerable(true); 
-                    
-                    world.spawnEntity(itemEntity);
-                    SSoggySoulsMod.LOGGER.info("Dropped {}'s head at {} {} {}", player.getName().getString(), pos.getX(), pos.getY(), pos.getZ());
+                        // Spawn item entity
+                        ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, head);
+                        itemEntity.setInvulnerable(true); 
+                        world.spawnEntity(itemEntity);
+                        SSoggySoulsMod.LOGGER.info("Dropped {}'s head at {} {} {}", player.getName().getString(), pos.getX(), pos.getY(), pos.getZ());
+                    }
                 });
             });
         });
+    }
+
+    private static BlockPos findSafeBlockPos(World world, BlockPos origin) {
+        BlockPos.Mutable mutable = origin.mutableCopy();
+        // Search up to 10 blocks up for air
+        for (int i = 0; i < 10; i++) {
+            if (world.getBlockState(mutable).isAir()) return mutable.toImmutable();
+            mutable.move(0, 1, 0);
+        }
+        return origin;
     }
 }
