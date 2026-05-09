603479f3e89e6d1ce95b60aae24e11aa1aa496bc
Maaren <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-09 10:25:43 +0200

    refactor(fabric): resolve ide compiler warnings
    - resolve unused readLastLines warning in CommandRegistration
    - suppress unused field warning for db in MainServerListener
    - replace deprecated config methods in DlcCommandRegistration

--- fabric/src/main/java/org/ssoggy/ssoggysouls/listener/MainServerListener.java
+++ fabric/src/main/java/org/ssoggy/ssoggysouls/listener/MainServerListener.java
@@ -29,6 +29,7 @@ import java.util.concurrent.CompletableFuture;
  */
 public class MainServerListener {
 
+    @SuppressWarnings("unused")
     private final DatabaseManager db;
 
     private MainServerListener(DatabaseManager db) {
