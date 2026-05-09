b082b63db2b7f5d44523e00e64d4eab857a5981d
Maaren <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-09 10:29:48 +0200

    refactor(fabric): remove deprecated config methods
    - remove all deprecated blocktag and headrestrict getters and setters
      from ConfigManager
    - update RevivalStructureListener to use non-deprecated camelCase
      getters, resolving all SonarCloud warnings

--- fabric/src/main/java/org/ssoggy/ssoggysouls/util/ConfigManager.java
+++ fabric/src/main/java/org/ssoggy/ssoggysouls/util/ConfigManager.java
@@ -219,8 +219,6 @@ public class ConfigManager {
         public boolean isLoseInventory() { return loseInventory; }
         public boolean isGhostModeParticles() { return ghostModeParticles; }
         public int getSpectatorHeadRestrictRadius() { return spectatorHeadRestrictRadius; }
-        @Deprecated
-        public int getSpectatorHeadrestrictRadius() { return getSpectatorHeadRestrictRadius(); }
         public boolean isRestrictMenuAccess() { return restrictMenuAccess; }
         public boolean isCreativePlayersDropHeads() { return creativePlayersDropHeads; }
         public boolean isHeadBurnsInLava() { return headBurnsInLava; }
@@ -236,16 +234,6 @@ public class ConfigManager {
         public java.util.List<String> getOreBlockTag() { return oreBlockTag; }
         public java.util.List<String> getFenceBlockTag() { return fenceBlockTag; }
         public java.util.List<String> getStairBlockTag() { return stairBlockTag; }
-        @Deprecated
-        public java.util.List<String> getSoulSandBlocktag() { return getSoulSandBlockTag(); }
-        @Deprecated
-        public java.util.List<String> getFlowerBlocktag() { return getFlowerBlockTag(); }
-        @Deprecated
-        public java.util.List<String> getOreBlocktag() { return getOreBlockTag(); }
-        @Deprecated
-        public java.util.List<String> getFenceBlocktag() { return getFenceBlockTag(); }
-        @Deprecated
-        public java.util.List<String> getStairBlocktag() { return getStairBlockTag(); }
         public boolean isDebug() { return debug; }
         public boolean isCheckForUpdates() { return checkForUpdates; }
 
@@ -327,8 +315,6 @@ public class ConfigManager {
         public void setLoseInventory(boolean lose) { loseInventory = lose; }
         public void setGhostModeParticles(boolean particles) { ghostModeParticles = particles; }
         public void setSpectatorHeadRestrictRadius(int radius) { spectatorHeadRestrictRadius = radius; }
-        @Deprecated
-        public void setSpectatorHeadrestrictRadius(int radius) { setSpectatorHeadRestrictRadius(radius); }
         public void setRestrictMenuAccess(boolean restrict) { restrictMenuAccess = restrict; }
         public void setCreativePlayersDropHeads(boolean drop) { creativePlayersDropHeads = drop; }
         public void setHeadBurnsInLava(boolean burns) { headBurnsInLava = burns; }
