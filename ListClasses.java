bcd179430fba9d9e6d540bb4182fd93aded5286f
Maaren <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-10 10:58:48 +0200

    chore(common): replace machine-specific path in ListClasses helper
    - use user.home property instead of hardcoded home directory

--- ListClasses.java
+++ ListClasses.java
@@ -5,7 +5,13 @@ import java.util.Enumeration;
 
 public class ListClasses {
     public static void main(String[] args) throws Exception {
-        try (ZipFile zf = new ZipFile("/home/jules/.gradle/caches/forge_gradle/minecraft_user_repo/net/minecraftforge/forge/1.21.1-52.1.0_mapped_official_1.21.1/forge-1.21.1-52.1.0_mapped_official_1.21.1-srg.jar")) {
+        String path = System.getProperty("user.home") + "/.gradle/caches/forge_gradle/minecraft_user_repo/net/minecraftforge/forge/1.21.1-52.1.0_mapped_official_1.21.1/forge-1.21.1-52.1.0_mapped_official_1.21.1-srg.jar";
+        File jarFile = new File(path);
+        if (!jarFile.exists()) {
+            System.err.println("Could not find jar at: " + path);
+            return;
+        }
+        try (ZipFile zf = new ZipFile(jarFile)) {
             Enumeration<? extends ZipEntry> entries = zf.entries();
             while (entries.hasMoreElements()) {
                 ZipEntry entry = entries.nextElement();
