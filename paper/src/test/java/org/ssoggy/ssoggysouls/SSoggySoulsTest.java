a958b2aa80ed70d1d0bd4c2fe177b30fbc251490
SSoggyTacoMan <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-20 12:47:20 +0000

    refactor(paper): refactor onEnable method to improve readability

--- paper/src/test/java/org/ssoggy/ssoggysouls/SSoggySoulsTest.java
+++ paper/src/test/java/org/ssoggy/ssoggysouls/SSoggySoulsTest.java
@@ -9,7 +9,6 @@ import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class SSoggySoulsTest {
