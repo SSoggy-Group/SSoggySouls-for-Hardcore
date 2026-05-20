e097f0436724da404f9ce3d0732c7a8fb6c43463
SSoggyTacoMan <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-20 12:15:03 +0000

    refactor(forge): remove unused import in ghostmodeevents

--- forge/src/main/java/org/ssoggy/ssoggysouls/hrm/dlc/listener/GhostModeEvents.java
+++ forge/src/main/java/org/ssoggy/ssoggysouls/hrm/dlc/listener/GhostModeEvents.java
@@ -14,7 +14,6 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.ssoggy.ssoggysouls.SSoggySoulsMod;
 import org.ssoggy.ssoggysouls.database.DatabaseManager;
 import org.ssoggy.ssoggysouls.hrm.dlc.util.GhostState;
 import org.ssoggy.ssoggysouls.hrm.dlc.shared.GhostRestrictionLogic;
