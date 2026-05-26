6445f45057c7ee4b8a0f4a759842e5e2bcbed34c
SSoggyTacoMan <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-05-26 12:44:25 +0000

    ci(github): fix setup-java action resolution failure
    - Updated pinned sha for actions/setup-java to v4 since the pinned sha was returning a 404.

--- paper/src/main/java/org/ssoggy/ssoggysouls/hrm/dlc/commands/ObituariesCommand.java
+++ paper/src/main/java/org/ssoggy/ssoggysouls/hrm/dlc/commands/ObituariesCommand.java
@@ -29,7 +29,6 @@ import org.ssoggy.ssoggysouls.hrm.dlc.util.RPSocial;
 import org.ssoggy.ssoggysouls.hrm.dlc.util.RPStatic;
 import org.ssoggy.ssoggysouls.hrm.dlc.util.RPUtil;
 import org.ssoggy.ssoggysouls.hrm.dlc.util.Pair;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -96,7 +95,7 @@ public class ObituariesCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
+        return org.ssoggy.ssoggysouls.util.TabCompleteUtil.getOnlinePlayerNames(args.length > 0 ? args[0] : "");
     }
 
     private static long getObituaryDelayMinutes(FileConfiguration config, String key, long fallback) {
