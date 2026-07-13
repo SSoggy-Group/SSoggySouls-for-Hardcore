0e463ca164382570d3214cabd83492566d663907
SSoggyTacoMan <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-07-13 09:56:41 +0000

    refactor(paper): ⚡ bolt: eliminate gc pressure and redundant disk io in configs

---
+++ paper/src/test/java/org/ssoggy/ssoggysouls/hrm/dlc/util/RPCommandOutputTest.java
@@ -0,0 +1,33 @@
+package org.ssoggy.ssoggysouls.hrm.dlc.util;
+
+import org.junit.jupiter.api.Test;
+import org.ssoggy.ssoggysouls.hrm.dlc.enums.COMMANDOUTPUTENUM;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RPCommandOutputTest {
+
+    @Test
+    void testToString() {
+        RPCommandOutput output = new RPCommandOutput();
+        output.success = COMMANDOUTPUTENUM.TRUE;
+        output.message = "Test message";
+        assertEquals("\n" + RPStatic.PREFIX + " <green>Success! Test message</green>\n", output.toString());
+    }
+
+    @Test
+    void testToStringFalse() {
+        RPCommandOutput output = new RPCommandOutput();
+        output.success = COMMANDOUTPUTENUM.FALSE;
+        output.message = "Test message";
+        assertEquals("\n" + RPStatic.PREFIX + " <red>Failure. Test message</red>\n", output.toString());
+    }
+
+    @Test
+    void testToStringInfo() {
+        RPCommandOutput output = new RPCommandOutput();
+        output.success = COMMANDOUTPUTENUM.INFO;
+        output.message = "Test message";
+        assertEquals("\n" + RPStatic.PREFIX + " <gray>Test message</gray>\n", output.toString());
+    }
+}
---
+++ paper/src/test/java/org/ssoggy/ssoggysouls/hrm/dlc/util/RPConfigTest.java
@@ -0,0 +1,13 @@
+package org.ssoggy.ssoggysouls.hrm.dlc.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class RPConfigTest {
+
+    @Test
+    void testDummy() {
+        assertNotNull(RPConfig.class);
+    }
+}
